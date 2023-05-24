9479e890201408c9c734c4a677ca86c6813b8a42
px <511055984@qq.com>
2023-05-24 21:16:14 +0800

    新增：编辑模版布局按钮

--- tianjy_report/tianjy_report/doctype/tianjy_report_template/tianjy_report_template.js
+++ tianjy_report/tianjy_report/doctype/tianjy_report_template/tianjy_report_template.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Tianjy Report Template', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function (frm) {
+		if (!frm.is_new()) {
+			frm.add_custom_button(__('Edit Layout'), function () {
+				const a = document.createElement('a');
+				a.href = `/app/tianjy-report-page?name=${frm.doc.name}&mode=template`;
+				a.click();
+			});
+		}
+	}
 });
