1fdba8b7b09e0f1a60684e159fdf0ec92af4127f
px <511055984@qq.com>
2023-05-26 13:06:35 +0800

    新增： 添加固定工具栏，删除气泡工具栏

---
+++ tianjy_report/public/js/tianjy_report/page/tianjy_report/components/Tools.vue
@@ -0,0 +1,102 @@
+<template>
+	<div class="tools container" v-if="editor">
+		<ElButton :class="{ 'is-active': editor.isActive('bold') }"
+			@click="editor.chain().focus().toggleBold().run()">
+			<Bold :size="16"></Bold>
+		</ElButton>
+		<ElButton :class="{ 'is-active': editor.isActive('italic') }"
+			@click="editor.chain().focus().toggleItalic().run()">
+			<Italic :size="16"></Italic>
+		</ElButton>
+		<ElButton :class="{ 'is-active': editor.isActive('strike') }"
+			@click="editor.chain().focus().toggleStrike().run()">
+			<Strikethrough :size="16"></Strikethrough>
+		</ElButton>
+		<ElButton @click="editor.chain().focus().setTextAlign('left').run()">
+			<AlignLeft :size="16"></AlignLeft>
+		</ElButton>
+		<ElButton @click="editor.chain().focus().setTextAlign('center').run()">
+			<AlignCenter :size="16"></AlignCenter>
+		</ElButton>
+		<ElButton @click="editor.chain().focus().setTextAlign('justify').run()">
+			<AlignJustify :size="16"></AlignJustify>
+		</ElButton>
+		<ElButton @click="editor.chain().focus().setTextAlign('right').run()">
+			<AlignRight :size="16"></AlignRight>
+		</ElButton>
+		<ElButton @click="editor.chain().focus()
+					.insertTable({ rows: 3, cols: 3, withHeaderRow: true })
+					.run();">
+			<Table :size="16"></Table>
+		</ElButton>
+		<ElButton @click="editor.chain().focus()
+					.setNode('heading', { level: 2 })
+					.run();">
+			<Heading1 :size="16"></Heading1>
+		</ElButton>
+		<ElButton @click="editor.chain().focus()
+					.setNode('heading', { level: 3 })
+					.run();">
+			<Heading2 :size="16"></Heading2>
+		</ElButton>
+		<ElButton @click="editor.chain().focus()
+					.setNode('heading', { level: 4 })
+					.run();">
+			<Heading3 :size="16"></Heading3>
+		</ElButton>
+		<ElButton @click="()=>{
+				const element = '<chart></chart>';
+				editor.chain().focus()
+					.insertContent(element)
+					.run();}">
+			<LineChart :size="16"></LineChart>
+		</ElButton>
+		<ElButton @click="insetImage">
+			<Image :size="16"></Image>
+		</ElButton>
+		<ElButton @click="editor.chain().focus().toggleOrderedList().run();">
+			<ListOrdered :size="16"></ListOrdered>
+		</ElButton>
+		<ElButton @click="editor.chain().focus().toggleBulletList().run();">
+			<List :size="16"></List>
+		</ElButton>
+	</div>
+</template>
+
+<script setup lang='ts'>
+import { ref, defineProps, defineEmits } from 'vue';
+
+import {
+	Bold, Italic, Strikethrough, AlignLeft, AlignCenter, AlignRight, AlignJustify, Table,
+	Heading1, Heading2, Heading3, LineChart, Image, ListOrdered, List,
+} from 'lucide-vue-next';
+interface Props{
+	editor:any
+}
+const props = defineProps<Props>();
+interface Emit{
+
+}
+const emit = defineEmits<Emit>();
+
+function insetImage(){
+	const input = document.createElement('input');
+		input.setAttribute('type', 'file');
+		input.setAttribute('accept', 'image/*');
+		input.addEventListener('change', e => {
+			if (!e.target?.files || e.target.files?.length === 0) { return; }
+			const file = e.target.files[0];
+			const reader = new FileReader();
+			reader.readAsDataURL(file);
+			reader.onload = function () {
+				const url = reader.result;
+				props.editor.chain().focus().setImage({ src: url })
+					.run();
+			};
+		});
+		input.click();
+}
+</script>
+
+<style lang='less' scoped>
+</style>
