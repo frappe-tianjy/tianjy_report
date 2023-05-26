a1e08b0deb0288a674a930625b5f662ff5a3072a
px <511055984@qq.com>
2023-05-26 16:11:38 +0800

    新增：按钮tooltip

--- tianjy_report/public/js/tianjy_report/page/tianjy_report/components/Tools.vue
+++ tianjy_report/public/js/tianjy_report/page/tianjy_report/components/Tools.vue
@@ -1,65 +1,95 @@
 <template>
 	<div class="tools container" v-if="editor">
-		<ElButton :class="{ 'is-active': editor.isActive('bold') }"
-			@click="editor.chain().focus().toggleBold().run()">
-			<Bold :size="16"></Bold>
-		</ElButton>
-		<ElButton :class="{ 'is-active': editor.isActive('italic') }"
-			@click="editor.chain().focus().toggleItalic().run()">
-			<Italic :size="16"></Italic>
-		</ElButton>
-		<ElButton :class="{ 'is-active': editor.isActive('strike') }"
-			@click="editor.chain().focus().toggleStrike().run()">
-			<Strikethrough :size="16"></Strikethrough>
-		</ElButton>
-		<ElButton @click="editor.chain().focus().setTextAlign('left').run()">
-			<AlignLeft :size="16"></AlignLeft>
-		</ElButton>
-		<ElButton @click="editor.chain().focus().setTextAlign('center').run()">
-			<AlignCenter :size="16"></AlignCenter>
-		</ElButton>
-		<ElButton @click="editor.chain().focus().setTextAlign('justify').run()">
-			<AlignJustify :size="16"></AlignJustify>
-		</ElButton>
-		<ElButton @click="editor.chain().focus().setTextAlign('right').run()">
-			<AlignRight :size="16"></AlignRight>
-		</ElButton>
-		<ElButton @click="editor.chain().focus()
-					.insertTable({ rows: 3, cols: 3, withHeaderRow: true })
-					.run();">
-			<Table :size="16"></Table>
-		</ElButton>
-		<ElButton @click="editor.chain().focus()
-					.setNode('heading', { level: 2 })
-					.run();">
-			<Heading1 :size="16"></Heading1>
-		</ElButton>
-		<ElButton @click="editor.chain().focus()
-					.setNode('heading', { level: 3 })
-					.run();">
-			<Heading2 :size="16"></Heading2>
-		</ElButton>
-		<ElButton @click="editor.chain().focus()
-					.setNode('heading', { level: 4 })
-					.run();">
-			<Heading3 :size="16"></Heading3>
-		</ElButton>
-		<ElButton @click="()=>{
-				const element = '<chart></chart>';
-				editor.chain().focus()
-					.insertContent(element)
-					.run();}">
-			<LineChart :size="16"></LineChart>
-		</ElButton>
-		<ElButton @click="insetImage">
-			<Image :size="16"></Image>
-		</ElButton>
-		<ElButton @click="editor.chain().focus().toggleOrderedList().run();">
-			<ListOrdered :size="16"></ListOrdered>
-		</ElButton>
-		<ElButton @click="editor.chain().focus().toggleBulletList().run();">
-			<List :size="16"></List>
-		</ElButton>
+		<el-tooltip content="加粗">
+			<ElButton :class="{ 'is-active': editor.isActive('bold') }"
+				@click="editor.chain().focus().toggleBold().run()">
+				<Bold :size="16"></Bold>
+			</ElButton>
+		</el-tooltip>
+		<el-tooltip content="斜体">
+			<ElButton :class="{ 'is-active': editor.isActive('italic') }"
+				@click="editor.chain().focus().toggleItalic().run()">
+				<Italic :size="16"></Italic>
+			</ElButton>
+		</el-tooltip>
+		<el-tooltip content="删除线">
+			<ElButton :class="{ 'is-active': editor.isActive('strike') }"
+				@click="editor.chain().focus().toggleStrike().run()">
+				<Strikethrough :size="16"></Strikethrough>
+			</ElButton>
+		</el-tooltip>
+		<el-tooltip content="左对齐">
+			<ElButton @click="editor.chain().focus().setTextAlign('left').run()">
+				<AlignLeft :size="16"></AlignLeft>
+			</ElButton>
+		</el-tooltip>
+		<el-tooltip content="中间对齐">
+			<ElButton @click="editor.chain().focus().setTextAlign('center').run()">
+				<AlignCenter :size="16"></AlignCenter>
+			</ElButton>
+		</el-tooltip>
+		<el-tooltip content="自动对齐">
+			<ElButton @click="editor.chain().focus().setTextAlign('justify').run()">
+				<AlignJustify :size="16"></AlignJustify>
+			</ElButton>
+		</el-tooltip>
+		<el-tooltip content="右对齐">
+			<ElButton @click="editor.chain().focus().setTextAlign('right').run()">
+				<AlignRight :size="16"></AlignRight>
+			</ElButton>
+		</el-tooltip>
+		<el-tooltip content="插入表格">
+			<ElButton @click="editor.chain().focus()
+						.insertTable({ rows: 3, cols: 3, withHeaderRow: true })
+						.run();">
+				<Table :size="16"></Table>
+			</ElButton>
+		</el-tooltip>
+		<el-tooltip content="标题1">
+			<ElButton @click="editor.chain().focus()
+						.setNode('heading', { level: 2 })
+						.run();">
+				<Heading1 :size="16"></Heading1>
+			</ElButton>
+		</el-tooltip>
+		<el-tooltip content="标题2">
+			<ElButton @click="editor.chain().focus()
+						.setNode('heading', { level: 3 })
+						.run();">
+				<Heading2 :size="16"></Heading2>
+			</ElButton>
+		</el-tooltip>
+		<el-tooltip content="标题3">
+			<ElButton @click="editor.chain().focus()
+						.setNode('heading', { level: 4 })
+						.run();">
+				<Heading3 :size="16"></Heading3>
+			</ElButton>
+		</el-tooltip>
+		<el-tooltip content="图表">
+			<ElButton @click="()=>{
+					const element = '<chart></chart>';
+					editor.chain().focus()
+						.insertContent(element)
+						.run();}">
+				<LineChart :size="16"></LineChart>
+			</ElButton>
+		</el-tooltip>
+		<el-tooltip content="插入图片">
+			<ElButton @click="insetImage">
+				<Image :size="16"></Image>
+			</ElButton>
+		</el-tooltip>
+		<el-tooltip content="有序列表">
+			<ElButton @click="editor.chain().focus().toggleOrderedList().run();">
+				<ListOrdered :size="16"></ListOrdered>
+			</ElButton>
+		</el-tooltip>
+		<el-tooltip content="无序列表">
+			<ElButton @click="editor.chain().focus().toggleBulletList().run();">
+				<List :size="16"></List>
+			</ElButton>
+		</el-tooltip>
 	</div>
 </template>
 
